7c6cb0d842867f42445d1bbdafcffe908a55ba55
Saddam Hussain <saddyhussain7503@gmail.com>
2025-12-13 11:18:23 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
